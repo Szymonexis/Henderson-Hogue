270b9e5fcc1c167ca2ffbdb05053a1b6e336e6e2
Szymonexis <67708724+Szymonexis@users.noreply.github.com>
2021-11-02 00:43:38 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/"></Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/MainTable/MainTable.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MainTable = () => {
+  return <table>
+      <tr>
+          <th></th>
+          <th></th>
+      </tr>
+      <tr>
+          <td></td>
+          <td></td>
+      </tr>
+  </table>;
+};
+
+export default MainTable;
